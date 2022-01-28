6c7b25deb9c2d03bba7b28043ed2b60e347e9d38
Michael Cartee <admin@asgardcrolingai.lan>
2022-01-27 19:01:42 -0500

    Finished the excercise

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign="left"
+let dogImages = document.querySelectorAll('.dog-image')
+for (let i=0; i<dogImages.length; i++){
+    dogImages[i].style.border='5px SOLID #555'
+}
+let dogName= document.querySelectorAll('.dog-name')
+for (let i=0; i<dogName.length; i++){
+    dogName[i].style.textAlign="left"
+}
